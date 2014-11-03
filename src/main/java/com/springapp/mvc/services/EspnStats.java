e2f88c2ab9e24d814ea1ec2623027ca00dd2a232
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2014-11-03 10:33:59 +0000

    commit fix

---
+++ src/main/java/com/springapp/mvc/services/EspnStats.java
@@ -0,0 +1,45 @@
+package com.springapp.mvc.services;
+
+import com.springapp.mvc.dataStructures.TableEntry;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Stats taken from ESPN.com
+ */
+public class EspnStats {
+
+    private HashMap<Integer, TableEntry> table = new HashMap<Integer, TableEntry>();
+    private final String urlOfTable ="http://www.espnfc.com/barclays-premier-league/23/table";
+
+    public EspnStats(){
+        getData();
+    }
+
+    private boolean getData(){
+        try {
+            Document doc = Jsoup.connect(urlOfTable).get();
+            Elements teamNames = doc.getElementsByAttributeValue("class", "team").not("th");
+            Elements points = doc.getElementsByAttributeValue("class", "pts").not("th");
+            int count =0;
+            for (Element name : teamNames ){
+                int numPoints = Integer.parseInt(points.get(count).text());
+                TableEntry tableEntry = new TableEntry(name.text(),numPoints);
+                table.put(count+1, tableEntry);
+                count++;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public HashMap<Integer, TableEntry> getTable(){
+        return table;
+    }
+}
---
+++ src/main/java/com/springapp/mvc/services/PremierLeagueStats.java
@@ -0,0 +1,7 @@
+package com.springapp.mvc.services;
+
+/**
+ * Stats taken from PremierLeague.com
+ */
+public class PremierLeagueStats {
+}
